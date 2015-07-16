5da969bc7b11342ab4fd9b6e1f0e89c22a0db626
piotrkot <piotr.kotlicki@gmail.com>
2015-07-16 15:41:22 +0200

    Adding tests

---
+++ src/test/java/com/piokot/cli/CommandLineArgsTest.java
@@ -0,0 +1,202 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 piotrkot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.piokot.cli;
+
+import java.util.Iterator;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests for {@link CommandLineArgs} class.
+ *
+ * @author Piotr Kotlicki (piotr.kotlicki@gmail.com)
+ * @version $Id$
+ * @since 1.0
+ */
+public final class CommandLineArgsTest {
+    /**
+     * Can get options when there are none.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void getOptionsWhenNone() throws Exception {
+        Assert.assertFalse(
+            "Options found",
+            new CommandLineArgs().getOptions().iterator().hasNext()
+        );
+    }
+
+    /**
+     * Can get all options.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void getAllOptions() throws Exception {
+        final String file = "file.txt";
+        final Iterator<Option> options = new CommandLineArgs(
+            "-x",
+            "-fvx",
+            "--help",
+            "--max-depth=3",
+            "-f",
+            file,
+            "-D",
+            "par=10"
+        ).getOptions().iterator();
+        Assert.assertEquals(
+            "Short option incorrect",
+            "x",
+            options.next().value()
+        );
+        Assert.assertEquals(
+            "Short POSIX option incorrect",
+            "fvx",
+            options.next().value()
+        );
+        Assert.assertEquals(
+            "Long option incorrect",
+            "help",
+            options.next().value()
+        );
+        Assert.assertEquals(
+            "Short with param option incorrect",
+            "3",
+            options.next().value()
+        );
+        final Option withfile = options.next();
+        Assert.assertEquals(
+            "Short with arg option incorrect",
+            "f",
+            withfile.value()
+        );
+        Assert.assertEquals(
+            "Short with arg argument incorrect",
+            file,
+            withfile.arguments().iterator().next()
+        );
+        Assert.assertEquals(
+            "Long with param option incorrect",
+            "10",
+            options.next().value()
+        );
+    }
+
+    /**
+     * Can find POSIX option.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void findPOSIXOption() throws Exception {
+        final String tar = "foo.tar.gz";
+        final Option option = new CommandLineArgs("-zxvf", tar)
+            .getOptions().iterator().next();
+        Assert.assertEquals("Posix opt found", "zxvf", option.value());
+        Assert.assertEquals(
+            "Tar file not found",
+            tar,
+            option.arguments().iterator().next()
+        );
+    }
+
+    /**
+     * Can find GNU option.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void findGNUOption() throws Exception {
+        final CommandLineArgs cli = new CommandLineArgs(
+            "--human-readable",
+            "--max-depth=1"
+        );
+        Assert.assertTrue(
+            "Gnu opt not found",
+            cli.findOption("human-readable").iterator().hasNext()
+        );
+        Assert.assertEquals(
+            "Gnu opt value not found",
+            "1",
+            cli.findOption("max-depth").iterator().next().value()
+        );
+    }
+
+    /**
+     * Can find Java-like option.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void findJavaLikeOption() throws Exception {
+        final String truee = "true";
+        final CommandLineArgs cli = new CommandLineArgs(
+            "-Djava.awt.headless=true",
+            "-Djava.net.useSystemProxies=true"
+        );
+        final Iterator<Option> iter = cli.findOption("D").iterator();
+        Option opt = iter.next();
+        Assert.assertEquals(
+            "Java like opt 1 key not found",
+            "java.awt.headless",
+            opt.key()
+        );
+        Assert.assertEquals(
+            "Java like opt 1 value not found",
+            truee,
+            opt.value()
+        );
+        opt = iter.next();
+        Assert.assertEquals(
+            "Java like opt 2 key not found",
+            "java.net.useSystemProxies",
+            opt.key()
+        );
+        Assert.assertEquals(
+            "Java like opt 2 value not found",
+            truee,
+            opt.value()
+        );
+    }
+    /**
+     * Can find short option with value.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void findShortOptionWithValue() throws Exception {
+        final CommandLineArgs cli = new CommandLineArgs("-O2");
+        Assert.assertEquals(
+            "Opt not found by short name",
+            "2",
+            cli.findOption("O").iterator().next().value()
+        );
+        Assert.assertEquals(
+            "Opt not found by all",
+            "O2",
+            cli.getOptions().iterator().next().value()
+        );
+    }
+}
---
+++ src/test/java/com/piokot/cli/OptionTest.java
@@ -0,0 +1,200 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 piotrkot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.piokot.cli;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests for {@link Option} class.
+ *
+ * @author Piotr Kotlicki (piotr.kotlicki@gmail.com)
+ * @version $Id$
+ * @since 1.0
+ */
+public final class OptionTest {
+    /**
+     * Can return empty arguments when there are none.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void returnEmptyArguments() throws Exception {
+        Assert.assertFalse(
+            "Arguments exist",
+            new Option("empty", Collections.<String>emptyList()).arguments()
+                .iterator().hasNext()
+        );
+    }
+    /**
+     * Can return one argument with parameter defined afterwards.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void returnOneArgumentWithParamAfter() throws Exception {
+        final String file = "filee";
+        final Iterator<String> args = new Option(
+            "a",
+            Arrays.asList(file, "par=10")
+        ).arguments().iterator();
+        Assert.assertEquals(
+            "Argument file correct",
+            file,
+            args.next()
+        );
+        Assert.assertFalse("Param after is an argument", args.hasNext());
+    }
+    /**
+     * Can return one argument with parameter defined before.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void returnOneArgumentWithParamBefore() throws Exception {
+        final String file = "newfile";
+        final Iterator<String> args = new Option(
+            "b",
+            Arrays.asList("pp=xx", file)
+        ).arguments().iterator();
+        Assert.assertEquals(
+            "Argument newfile correct",
+            file,
+            args.next()
+        );
+        Assert.assertFalse("Param before is an argument", args.hasNext());
+    }
+    /**
+     * Can return two arguments.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void returnTwoArguments() throws Exception {
+        final String fone = "newfile1";
+        final String ftwo = "newfile2";
+        final Iterator<String> args = new Option(
+            "two",
+            Arrays.asList(fone, ftwo)
+        ).arguments().iterator();
+        Assert.assertEquals(
+            "Argument first correct",
+            fone,
+            args.next()
+        );
+        Assert.assertEquals(
+            "Argument second correct",
+            ftwo,
+            args.next()
+        );
+        Assert.assertFalse("More arguments", args.hasNext());
+    }
+    /**
+     * Can return empty value.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void returnEmptyValue() throws Exception {
+        Assert.assertTrue(
+            "Non empty value",
+            new Option("", Collections.<String>emptyList()).value().isEmpty()
+        );
+        Assert.assertTrue(
+            "Non empty value with arg",
+            new Option("", Collections.singletonList("arg")).value().isEmpty()
+        );
+    }
+    /**
+     * Can return single value.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void returnSingleValue() throws Exception {
+        final String val = "value";
+        Assert.assertEquals(
+            "Wrong value",
+            val,
+            new Option(val, Collections.<String>emptyList()).value()
+        );
+        Assert.assertEquals(
+            "Wrong value with arg",
+            val,
+            new Option(val, Collections.singletonList("ar")).value()
+        );
+    }
+    /**
+     * Can return empty key.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void returnEmptyKey() throws Exception {
+        Assert.assertTrue(
+            "Non empty key",
+            new Option("", Collections.<String>emptyList()).key().isEmpty()
+        );
+        Assert.assertTrue(
+            "Non empty key with arg",
+            new Option("", Collections.singletonList("key")).key().isEmpty()
+        );
+        Assert.assertTrue(
+            "Non empty key with value",
+            new Option("kval", Collections.singletonList("kk")).key().isEmpty()
+        );
+    }
+    /**
+     * Can return non empty key.
+     *
+     * @throws Exception When it fails.
+     */
+    @Test
+    public void returnKey() throws Exception {
+        final String key = "k";
+        Assert.assertEquals(
+            "Empty key",
+            "Kk",
+            new Option("K", Collections.singletonList("k=val")).key()
+        );
+        Assert.assertEquals(
+            "Empty key with arg",
+            key,
+            new Option("", Arrays.asList("k=vval", "file")).key()
+        );
+        Assert.assertEquals(
+            "Empty key with arg reordered",
+            key,
+            new Option("", Arrays.asList("file.x", "k=xx")).key()
+        );
+        Assert.assertEquals(
+            "Empty key in main part",
+            key,
+            new Option("k=xad", Collections.<String>emptyList()).key()
+        );
+    }
+}
